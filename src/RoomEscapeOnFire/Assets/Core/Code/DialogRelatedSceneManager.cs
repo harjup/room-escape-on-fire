94a5791f2285baa3ad9d1081bf8f5f4aa415def8
pharju <harjup13@gmail.com>
2017-01-04 21:44:03 -0500

    Import Super Text Mesh

---
+++ src/RoomEscapeOnFire/Assets/Core/Code/DialogRelatedSceneManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class DialogRelatedSceneManager : MonoBehaviour
+{
+    private GameObject _currentScene;
+
+	void Start()
+	{
+	    var closetPrefab = Resources.Load<GameObject>("Prefabs/Rooms/Closet");
+        SetupScene(closetPrefab);
+	}
+
+    public void SetupScene(GameObject scenePrefab)
+    {
+        if (_currentScene != null)
+        {
+            Destroy(_currentScene);
+        }
+
+        Instantiate(scenePrefab, Vector3.zero, Quaternion.identity);
+    }
+}
