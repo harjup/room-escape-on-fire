b3e6f0c17cc56bae6207351bfe4533c59bd38d7a
pharju <harjup13@gmail.com>
2017-01-05 18:52:54 -0500

    Add ability to click on objects and read relevant descriptions

---
+++ src/RoomEscapeOnFire/Assets/Core/Code/Clickable.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Clickable : MonoBehaviour
+{
+    public string TextNode;
+
+    public void OnMouseUpAsButton()
+    {
+        // TODO: Fire event through something that checks for whether we're in a "cutscene"
+        FindObjectOfType<Yarn.Unity.DialogueRunner>().StartDialogue(TextNode);
+    }
+}
