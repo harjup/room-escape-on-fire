cd984c7d16cb3ee38334fd5bbdb90934ba957158
pharju <harjup13@gmail.com>
2017-01-08 14:30:35 -0500

    Implement scrolling the screen left to right (specifically for livingroom)

---
+++ src/RoomEscapeOnFire/Assets/Core/Code/CursorTracker.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+public class CursorTracker : MonoBehaviour
+{
+    public bool CursorInLeftScrollRange()
+    {
+        var leftRange = Screen.width* _scrollPercentage;
+        return _cursorX <= leftRange;
+    }
+
+    public bool CursorInRightScrollRange()
+    {
+        var rightRange = Screen.width - (Screen.width * _scrollPercentage);
+        return _cursorX >= rightRange;
+    }
+
+    private float _scrollPercentage = .1f;
+    private float _cursorX;
+    
+    void Update()
+    {
+        _cursorX = Input.mousePosition.x;
+    }
+}
---
+++ src/RoomEscapeOnFire/Assets/Core/Code/ScrollableBackground.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+using Yarn.Unity;
+
+public class ScrollableBackground : MonoBehaviour
+{
+    private CursorTracker _cursorTracker;
+    private DialogueRunner _dialogueRunner;
+
+    private float leftEdge = 7.25f;
+    private float rightEdge = -7.16f;
+    
+
+    void Awake()
+	{
+	    _cursorTracker = FindObjectOfType<CursorTracker>();
+        _dialogueRunner = FindObjectOfType<DialogueRunner>();
+
+	}
+
+	void Update()
+    {
+	    if (_dialogueRunner.isDialogueRunning)
+	    {
+	        return;
+	    }
+
+	    if (_cursorTracker.CursorInLeftScrollRange() && transform.localPosition.x < leftEdge)
+	    {
+	        transform.localPosition = transform.localPosition.AddX(Time.smoothDeltaTime * 3f);
+	    }
+
+        if (_cursorTracker.CursorInRightScrollRange() && transform.localPosition.x > rightEdge)
+        {
+            transform.localPosition = transform.localPosition.AddX(Time.smoothDeltaTime * -3f);
+        }
+
+    }
+}
