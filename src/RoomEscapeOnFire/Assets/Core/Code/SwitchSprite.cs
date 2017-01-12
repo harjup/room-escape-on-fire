ac4b85fef44c4670050b7cb8652181450b2b12e8
harjup <harjup13@gmail.com>
2017-01-11 20:51:56 -0500

    Fridge opens, computer plugs in

---
+++ src/RoomEscapeOnFire/Assets/Core/Code/SwitchSprite.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Yarn.Unity;
+
+public class SwitchSprite : MonoBehaviour
+{
+    public Sprite NextSprite;
+
+    [YarnCommand("switch-sprite")]
+    public void DoTheSwitchSprite()
+    {
+        var spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = NextSprite;
+    }
+
+    [YarnCommand("disable")]
+    public void DisableCollisions()
+    {
+        var collider = GetComponent<BoxCollider2D>();
+        collider.enabled = false;
+    }
+}
