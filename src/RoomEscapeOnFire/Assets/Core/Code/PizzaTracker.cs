846e9b65b8651b0b333ecc73563cda052ee70b91
pharju <harjup13@gmail.com>
2017-01-15 19:22:04 -0500

    Add pizza tracker animation

---
+++ src/RoomEscapeOnFire/Assets/Core/Code/PizzaTracker.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Assets.Core.Code;
+using UnityEngine;
+
+public class PizzaTracker : MonoBehaviour
+{
+    [AwaitableYarnCommand("pizza")]
+    public IEnumerator PlayPizzaTracker()
+    {
+        var room = FindObjectOfType<Room>();
+        var pizzaTrackerPrefab = Resources.Load<GameObject>("Prefabs/OneOffs/Pizza_Gauge");
+        var res = Instantiate(pizzaTrackerPrefab);
+        res.transform.parent = room.transform;
+        yield return new WaitForSeconds(8f);
+        Destroy(res);
+    }
+
+    [AwaitableYarnCommand("police")]
+    public IEnumerator PlayPoliceTracker()
+    {
+        var room = FindObjectOfType<Room>();
+        var pizzaTrackerPrefab = Resources.Load<GameObject>("Prefabs/OneOffs/Pizza_Gauge");
+        var res = Instantiate(pizzaTrackerPrefab);
+        res.transform.parent = room.transform;
+        yield return new WaitForSeconds(8f);
+        Destroy(res);
+    }
+}
