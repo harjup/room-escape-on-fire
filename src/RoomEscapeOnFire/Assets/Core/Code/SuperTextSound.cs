e864bb95accbb973261b602123e6084e5071a9a6
pharju <harjup13@gmail.com>
2017-01-16 18:57:29 -0500

    Add some sound effects to the script~

---
+++ src/RoomEscapeOnFire/Assets/Core/Code/SuperTextSound.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SuperTextSound : MonoBehaviour
+{
+    private AudioSource _audioSource;
+
+    public AudioClip Fridge;
+    public AudioClip Bang;
+    public AudioClip Knock;
+    public AudioClip Blanket;
+
+    public void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    public void DoEvent(string s, TextInfo info)
+    {
+        if (s == "fridge")
+        {
+            _audioSource.PlayOneShot(Fridge, 1f);
+        }
+        else if (s == "bang")
+        {
+            _audioSource.PlayOneShot(Bang, .5f);
+        }
+        else if (s == "knock")
+        {
+            _audioSource.PlayOneShot(Knock, 1f);
+        }
+        else if (s == "blanket")
+        {
+            _audioSource.PlayOneShot(Blanket, 1f);
+        }
+    }
+}
