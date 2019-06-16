a2d4344e10eee34c5f51defc6fe1488e5f89b6cd
pharju <harjup13@gmail.com>
2019-06-16 11:25:05 -0400

    Update pizza tracker, rewrite references to this is fine and bee movie

--- src/RoomEscapeOnFire/Assets/Core/Code/EndingName.cs
+++ src/RoomEscapeOnFire/Assets/Core/Code/EndingName.cs
@@ -11,34 +11,34 @@ public class EndingName : MonoBehaviour
         "Out_Of_Time",
         "Brute_Force",
         "Frozen",
-        "Bee_Movie",
-        "This_Is_Not_Fine",
+        "Funny_Bug_Movie",
+        "This_Is_Not_Cool",
         "Pizza",
         "Vore",
         "Rejection_Of_Vore",
     };
-    
-	void Start()
-	{
-	    var superText = GetComponent<SuperTextMesh>();
-        
-	    var progressStore = FindObjectOfType<EndingProgressStore>();
 
-	    var endingTexts = new List<string>();
-	    for (int i = 0; i < _endings.Length; i++)
-	    {
-	        var key = _endings[i];
-	        var num = i + 1;
-	        if (progressStore.HasEnding(key))
-	        {
-	            endingTexts.Add(num + ". " + key.Replace('_', ' '));
-	        }
-	        else
-	        {
+    void Start()
+    {
+        var superText = GetComponent<SuperTextMesh>();
+
+        var progressStore = FindObjectOfType<EndingProgressStore>();
+
+        var endingTexts = new List<string>();
+        for (int i = 0; i < _endings.Length; i++)
+        {
+            var key = _endings[i];
+            var num = i + 1;
+            if (progressStore.HasEnding(key))
+            {
+                endingTexts.Add(num + ". " + key.Replace('_', ' '));
+            }
+            else
+            {
                 endingTexts.Add(num + ". ???");
             }
-	    }
+        }
 
-	    superText.Text = string.Join(Environment.NewLine, endingTexts.ToArray());
-	}
+        superText.Text = string.Join(Environment.NewLine, endingTexts.ToArray());
+    }
 }
